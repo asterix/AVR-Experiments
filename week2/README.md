2edce117ec0a2bfec1b7fb4c4363c696d39164cd
desai043 <desai043@umn.edu>
2016-02-02 00:01:50 -0600

    Week 2

--- week2/README.md
+++ week2/README.md
@@ -26,3 +26,11 @@ Solution Comments
 * *Implemented Pin Change Interrupts for Buttons A and C*
 * *Replaced polling with interrupts.*
 
+* Menu: Which LEDs blink can be changed using button C presses
+        Button C presses             Function
+              0               Uses both external LEDs
+              1               Uses 1 external + yellow LED
+              2               Uses green + yellow LEDs
+              3               Uses green + 1 external LED
+
+* *Use Button A to make secondary LED blink 10 times*
--- week2/main.c
+++ week2/main.c
@@ -1,43 +1,171 @@
-/*-----------------------------------------------------
-This program makes on-board LEDs blink at a given
-rate. Blinking can be toggled using on-board buttons.
+/*-----------------------------------------------------------
+This program makes on-board and external LEDs blink at a 
+given rate depending on selections using buttons. LED used
+can be changed using on-board button C. 
+
 ISR -> Timers, ISR -> PCINTx
 
 Author:    desai043
-Created:   26-Jan-2015
+Created:   01-Feb-2015
 Hardware:  ATMEGA32U4 on A-Star 32U4 Robot
            Controller LV with Raspberry Pi Bridge
 
            Note: LFUSE = 0xFF, HFUSE = 0xD0
            XTAL = 16MHz (CKDIV8 = 1 => SYSCLK = 16MHz)
------------------------------------------------------*/
-#define F_CPU     16000000
+-----------------------------------------------------------*/
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-
-#define BUTTON_A PB3 // PCINT3
-#define BUTTON_B PD5
-#define BUTTON_C PB0 // PCINT0
-
-#define LED_YELLOW PORTC7
-#define LED_GREEN  PORTD5
-#define LED_RED    PORTB0
-
-#define NUM_MODES       3     /* Number of LED modes */
-#define DEBOUNCE_DELAY  20    /* Jitter time (ms) */
-
-/* HIGH and LOW for buttons */
-enum
-{
-   LOW = 0,
-   HIGH
-};
+#include "main.h"
 
 /* Globals */
 unsigned int tcounter = 0, button_a = 0, button_c = 0;
 unsigned char button_a_stat = LOW, button_c_stat = HIGH;
+
+
+
+/* Main */
+int main()
+{ 
+   /* Init ports and pins */
+   init_all();
+
+   /* Setup interrupts */
+   setup_interrupts();
+
+   /* Enable interrupts */
+   sei();
+
+   /* Main loop */
+   while(1)
+   {
+      /* 0.5Hz */
+      handle_blinking(1);
+      _delay_ms(1000);
+   }
+
+   return 0;
+}
+
+/* Change what to blink based on Button C */
+void handle_blinking(int where)
+{
+   switch(button_c + where)
+   {
+      case 0:
+      {
+         PORTC ^= (1 << LED_EXT2);
+         break;
+      }
+      case 1:
+      {
+         PORTD ^= (1 << LED_EXT1);
+         break;
+      }
+      case 2:
+      {
+         PORTC ^= (1 << LED_YELLOW);
+         break;
+      }
+      case 3:
+      {
+         PORTD ^= (1 << LED_GREEN);
+         break;
+      }
+      case 4:
+      {
+         PORTC ^= (1 << LED_EXT2);
+         break;
+      }
+      default:
+      {
+         /* Nothing */
+      }
+   }
+}
+
+/* Turn OFF all LEDs */
+void clear_all_leds()
+{
+   /* Turn OFF all LEDs */
+   PORTC &= ~((1 << LED_YELLOW) | (1 << LED_EXT2));
+   PORTD |= (1 << LED_GREEN);
+   PORTD &= ~(1 << LED_EXT1);
+}
+
+/*-----------------------------------------------------------
+                    INITIALIZATION
+-----------------------------------------------------------*/
+void init_all()
+{
+   /* Start up allowance */
+   _delay_ms(100);
+
+   /* Configure LED pins to output */
+   DDRC |= ((1 << LED_YELLOW)| (1 << LED_EXT2));
+   DDRD |= ((1 << LED_GREEN) | (1 << LED_EXT1));
+
+   /* Configure Button pins to input */
+   DDRB &= ~((1 << BUTTON_A) | (1 << BUTTON_C));
+
+   /* Enable pull-ups on input pins */
+   PORTB |= ((1 << BUTTON_A) | (1 << BUTTON_C));
+
+   /* Show a startup pattern */
+   startup_pattern_show();
+}
+
+void startup_pattern_show()
+{
+   int i;
+
+   /* Turn ON all LEDs */
+   PORTC |= (1 << LED_YELLOW);
+   PORTD |= (1 << LED_GREEN);
+
+   _delay_ms(200);
+
+   /* Flash LEDs 5 times */
+   for(i = 0; i <= 11; i++)
+   {
+      PORTC ^= (1 << LED_YELLOW);
+      PORTD ^= (1 << LED_GREEN);
+      
+      _delay_ms(100);
+   }
+
+   /* Turn OFF all LEDs */
+   PORTC &= ~(1 << LED_YELLOW);
+   PORTD |= (1 << LED_GREEN);
+}
+
+
+/*-----------------------------------------------------------
+                PERIPHERAL CONFIGURATION
+-----------------------------------------------------------*/
+
+/* Configure interrupts */
+void setup_interrupts()
+{
+   int result = 1;
+   /* Setup PCINTx interrupts for buttons */
+   result = setup_pcintx(PCINT3);
+
+   if(result)
+   {
+      result = setup_pcintx(PCINT0);
+   }
+
+   /* Timer 1 - 250ms interrupt generator */
+   if(result)
+   {
+      //setup_autoreload_timer1(250);
+   }
+
+   if(!result)
+   {
+      throw_error(ERR_CONFIG);
+   }
+}
+
 
 /* Timer 1 setup */
 int setup_autoreload_timer1(unsigned long int delay)
@@ -92,78 +220,51 @@ int setup_pcintx(unsigned char pcintx)
 }
 
 
-/*ISR - Timer 1 compare A interrupt */
-ISR(TIMER1_COMPA_vect)
-{
-   tcounter++;
 
-   /* Check button presses */
-   switch(button_a)
+/*-----------------------------------------------------------
+                 ERROR/EXCEPTION HANDLERS
+-----------------------------------------------------------*/
+
+void throw_error(error_code_t ec)
+{
+   /* Red LED */
+   cli();
+   DDRB |= (1 << LED_RED);
+
+   /* Show error and hang */
+   switch(ec)
    {
-      case 0:
+      case ERR_CONFIG:
       {
-         /* Green LED is pulled high, so the pin acts as a current sink */
-         /* Low turns ON green LED */
-         PORTD &= ~(1 << LED_GREEN);
+         PORTC |= (1 << LED_YELLOW);
          break;
       }
-      case 1:
+      case ERR_PERIPH:
       {
-         /* Blink */
-         PORTD ^= (1 << LED_GREEN);
-         break;
-      }
-      case 2:
-      {
-         /* High turns OFF green LED */
          PORTD |= (1 << LED_GREEN);
          break;
       }
       default:
       {
-         /* Nothing */
+         PORTC |= (1 << LED_YELLOW);
       }
    }
 
-   /* Button C */
-   switch(button_c)
-   {
-      case 0:
-      {
-         /* ON */
-         PORTC |= (1 << LED_YELLOW);
-         break;
-      }
-      case 1:
-      {
-         /* Blink */
-         if(tcounter >= 5)
-         {
-            PORTC ^= (1 << LED_YELLOW);
-            /* Mod 5 */
-           tcounter = 0;
-         }
-         break;
-      }
-      case 2:
-      {
-         /* OFF */
-         PORTC &= ~(1 << LED_YELLOW);
-         break;
-      }
-      default:
-      {
-         /* Nothing */
-      }
-   }
+   /* Turn red LED ON and hang */
+   PORTB |= (1 << LED_RED);
+   while(1);
 }
 
+
+/*-----------------------------------------------------------
+             INTERRUPT SERVICE ROUTINES
+-----------------------------------------------------------*/
 
 /* ISR - Pin Change Interrupt */
 /* All PCINTx detections are vectored here */
 ISR(PCINT0_vect)
 {
-   unsigned int button_a_now, button_c_now;
+   unsigned int button_a_now, button_c_now, it;
 
    if(PINB & (1 << BUTTON_A))
    {
@@ -197,6 +298,25 @@ ISR(PCINT0_vect)
    {
       _delay_ms(DEBOUNCE_DELAY);
       button_a_stat = HIGH;
+      
+      /* Do-not-disturb */
+      cli();
+
+      /* Turn off all */
+      clear_all_leds();
+      
+      for(it = 0; it < 20; it++)
+      {
+         /* Blink 10 times - 4Hz */
+         handle_blinking(0);
+         
+         /* 125ms = 64*31250 cycles @ 16MHz */
+         _delay_ms(125);
+      }
+
+      /* Turn off all */
+      clear_all_leds();
+      sei();
    }
 
    /* Button C */
@@ -236,37 +356,10 @@ ISR(PCINT0_vect)
 }
 
 
-int main()
-{ 
-   /* Start up allowance */
-   _delay_ms(100);
-
-   /* Configure LED pins to output */
-   DDRC |= (1 << LED_YELLOW);
-   DDRD |= (1 << LED_GREEN);
-
-   /* Configure Button pins to input */
-   DDRB &= ~((1 << BUTTON_A) | ((1 << BUTTON_C)));
-
-   /* Enable pull-ups on input pins */
-   PORTB |= ((1 << BUTTON_A) | (1 << BUTTON_C));
-
-   /* Setup PCINTx interrupts for buttons */
-   setup_pcintx(PCINT3);
-   setup_pcintx(PCINT0);
-
-   /* Timer 1 - 250ms interrupt generator */
-   setup_autoreload_timer1(250);
-
-   /* Enable interrupts */
-   sei();
-
-   /* Main loop */
-   while(1)
-   {
-      /* Nothing here */
-   }
-
-   return 0;
+/*ISR - Timer 1 compare A interrupt */
+ISR(TIMER1_COMPA_vect)
+{
+   /* Nothing here */
+   tcounter++;
 }
 
